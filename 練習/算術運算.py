040a28336ae34e8ad7172920d63bfa5c4bd5ce31
TheoYu <theo.yu@icloud.com>
2020-06-30 16:39:16 +0800

    算術運算

---
+++ 練習/算術運算.py
@@ -0,0 +1,16 @@
+'''
+Version: 0.1.0
+Author: TheoYu
+'''
+
+print(10 + 20)
+print(10 - 20)
+print(10 * 20)
+print(10 / 20)
+print(10 ** 20)
+print(10 // 20)
+print(10 % 20)
+
+print(2 % 2)
+print(2 /2)
+print(2 // 2)
