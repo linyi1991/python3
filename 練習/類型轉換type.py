fc44fc3f50d3931c416dcbad6230cf807f15ef00
TheoYu <theo.yu@icloud.com>
2020-06-30 15:09:13 +0800

    轉換類型

---
+++ 練習/類型轉換type.py
@@ -0,0 +1,24 @@
+'''
+Version: 0.1.0
+Author: TheoYu
+'''
+
+# 轉換類型
+
+a = 10
+b = 1.02
+c = 'hello word'
+d = True
+
+# 正常類型
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+# 改變類型
+print(float(a))
+print(int(b))
+print(bool(c))
+print(int(d))
+
