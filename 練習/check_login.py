1ae803c5e7dfc28a1886eaa1af8c298b56578d11
TheoYu <theo.yu@icloud.com>
2020-07-01 12:22:48 +0800

    簡易測試驗證密碼，if迴圈判別

---
+++ 練習/check_login.py
@@ -0,0 +1,13 @@
+'''
+Version: 0.1.0
+Author: TheoYu
+簡易測試驗證密碼，if迴圈判別
+'''
+account = 'root'
+pwd = '123456'
+acc = str(input('Please input username: '))
+password = str(input('Please input password: '))
+if account == 'root' and password == 'pwd':
+    print('Success login')
+else:
+    print('Please try again!')
